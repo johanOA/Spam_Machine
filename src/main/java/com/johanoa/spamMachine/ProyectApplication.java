3dd465ef50a151cb0e0d4f59aea10c653a5b59f4
johanOA <johana.ospinao@uqvirtual.edu.co>
2023-06-18 11:02:05 -0500

    First version of the project

--- src/main/java/com/johanoa/spamMachine/ProyectApplication.java
+++ src/main/java/com/johanoa/spamMachine/ProyectApplication.java
@@ -1,2 +1,11 @@
-package com.johanoa.spamMachine;public class ProyectApplication {
+package com.johanoa.spamMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectApplication.class, args);
+    }
 }
